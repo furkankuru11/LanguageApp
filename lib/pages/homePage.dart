0e339ded1e62b5948d271d42b4f3d1e798f95c90
Furkan Kuru <furkankuru11@outlook.com>
2023-12-28 19:01:31 +0300

    s

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,10 +12,24 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(child: Text("Anasayfa")),
-        color: Colors.red,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "İngilizce Sözlük",
+          style: GoogleFonts.notoSansOsmanya(
+              color: Colors.black, fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center,
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.drag_handle),
+          color: Colors.black,
+          onPressed: () {},
+        ),
       ),
+      body: SafeArea(
+          child: Container(
+        color: Colors.red,
+      )),
     );
   }
 }
