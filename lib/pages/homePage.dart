dce6c403f6f6b149e198391c8c5c4ef7d0519207
Furkan Kuru <furkankuru11@outlook.com>
2023-12-28 19:36:36 +0300

    Update homePage.dart

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -11,6 +11,9 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -27,9 +30,58 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       body: SafeArea(
-          child: Container(
-        color: Colors.red,
-      )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Column(
+              children: [
+                Container(
+                  width: width * 0.6,
+                  height: 50,
+                  color: Colors.red,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: width * 0.6,
+                  height: 50,
+                  color: Colors.blue,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: width * 0.6,
+                  height: 50,
+                  color: Colors.green,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.green,
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
