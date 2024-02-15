de6669d7c93284bfb361da7a3e5765b3817586e7
Furkan Kuru <furkankuru11@outlook.com>
2024-02-16 00:59:55 +0300

    s
    s

--- lib/pages/list.dart
+++ lib/pages/list.dart
@@ -12,6 +12,7 @@ class ListPage extends StatefulWidget {
 
 class _ListPageState extends State<ListPage> {
   List<Map<String, Object?>> _lists = [];
+  bool pressController = false;
 
   @override
   void initState() {
@@ -31,17 +32,16 @@ class _ListPageState extends State<ListPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => CreateList()));
-        },
-        child: Icon(
-          Icons.add,
-          color: Colors.white,
-          size: 25,
-        ),
-        backgroundColor: Colors.green,
-      ),
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => CreateList()));
+          },
+          child: Icon(
+            Icons.add,
+            color: Colors.white,
+            size: 25,
+          ),
+          backgroundColor: Color(0xFF003840)),
       appBar: AppBar(
         centerTitle: true,
         title: Text(
@@ -65,67 +65,102 @@ class _ListPageState extends State<ListPage> {
     );
   }
 
-  InkWell listItem(int id,
+  void delete() async {}
+
+  Widget listItem(int id,
       {@required String? listName,
       @required String? sumWords,
       @required String? sumUnlearned}) {
-    return InkWell(
-      onTap: () {
-        debugPrint(id.toString());
+    return Dismissible(
+      key: Key(id.toString()),
+      onDismissed: (direction) {
+        delete();
       },
-      child: Center(
-        child: Column(
-          children: [
-            Column(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(25),
-                    color: Colors.red,
-                  ),
-                  margin: EdgeInsets.only(top: 20, left: 20, right: 20),
-                  width: double.infinity,
-                  height: 130,
-                  child: Column(
-                    children: [
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        child: Column(
-                          children: [
-                            Text(
-                              listName!,
-                              style: TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            SizedBox(height: 3),
-                            Text(
-                              "${sumWords!}: Terim",
-                              style:
-                                  TextStyle(fontSize: 15, color: Colors.white),
-                            ),
-                            SizedBox(height: 3),
-                            Text(
-                              "${(int.parse(sumWords) - int.parse(sumUnlearned!))}: Kelime Öğrenildi",
-                              style:
-                                  TextStyle(fontSize: 15, color: Colors.white),
-                            ),
-                            SizedBox(height: 3),
-                            Text(
-                              "${sumUnlearned!}: Kelime Öğrenilecek",
-                              style:
-                                  TextStyle(fontSize: 15, color: Colors.white),
-                            ),
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ],
+      background: Container(
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: EdgeInsets.only(left: 20.0),
+            child: Icon(
+              Icons.delete_forever_outlined,
+              color: Color(0xFF02A676),
             ),
-          ],
+          ),
+        ),
+      ),
+      secondaryBackground: Container(
+        child: Align(
+          alignment: Alignment.centerRight,
+          child: Padding(
+            padding: EdgeInsets.only(right: 30.0),
+            child: Icon(
+              Icons.delete,
+              size: 30,
+              color: Color(0xFFD92525),
+            ),
+          ),
+        ),
+      ), // Sağa doğru kaydırma işlemini kaldırıyoruz
+      direction: DismissDirection
+          .endToStart, // Yalnızca sola doğru kaydırma izin veriliyor
+      child: InkWell(
+        onTap: () {
+          debugPrint(id.toString());
+          // Öğeye tıklandığında yapılacak işlemler buraya gelecek
+        },
+        child: Center(
+          child: Column(
+            children: [
+              Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25),
+                        color: Color(0xFF007369)),
+                    margin: EdgeInsets.only(top: 20, left: 20, right: 20),
+                    width: double.infinity,
+                    height: 130,
+                    child: Column(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: Column(
+                            children: [
+                              Text(
+                                listName!,
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(height: 3),
+                              Text(
+                                "${sumWords!}: Terim",
+                                style: TextStyle(
+                                    fontSize: 15, color: Colors.white),
+                              ),
+                              SizedBox(height: 3),
+                              Text(
+                                "${(int.parse(sumWords) - int.parse(sumUnlearned!))}: Kelime Öğrenildi",
+                                style: TextStyle(
+                                    fontSize: 15, color: Colors.white),
+                              ),
+                              SizedBox(height: 3),
+                              Text(
+                                "${sumUnlearned!}: Kelime Öğrenilecek",
+                                style: TextStyle(
+                                    fontSize: 15, color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
